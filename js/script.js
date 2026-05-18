117910c6cdbb88898b5372f81b1b3618e26590ad
Fabricio Aquiles <fab.aquiles123@gmail.com>
2026-05-18 08:50:01 -0300

    feat: adicionado modal para excecoes de erro

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 
 //Objeto INTERNO
 const usuarioDb = {
-    email:"email@email.com",
-    senha:"12345",
-    nome: "José Antonio",
-    avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
+  email: "email@email.com",
+  senha: "12345",
+  nome: "José Antonio",
+  avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
 }
 
 
@@ -14,81 +14,82 @@ const botaoEntrar = document.getElementById("btnEntrar");
 //Atrelando ao botão uma função que vai receber dois parâmetros.
 // O primeiro será o evento de tela, o segundo será a função de callBack ou função anônima.
 
-botaoEntrar.addEventListener("click", function(e){
-    
-    e.preventDefault();
+botaoEntrar.addEventListener("click", function (e) {
 
-    //Para casa:
-    //Capturar os campos de email e senha.
-    //Armazenar em variáveis.
-    //Imprimir seus valores.
-    const email = document.querySelector("#idEmail");
-    const senha = document.querySelector("#idSenha");
-    
-    //Armazenar os dados que chegam do form em um objeto:
-    const userForm = {
-        email: email.value,
-        senha: senha.value
+  e.preventDefault();
+
+  //Para casa:
+  //Capturar os campos de email e senha.
+  //Armazenar em variáveis.
+  //Imprimir seus valores.
+  const email = document.querySelector("#idEmail");
+  const senha = document.querySelector("#idSenha");
+  const divMsg = document.querySelector("#msg");
+  const modalDialog = document.querySelector("#meuModal");
+  const botaoModal = document.querySelector("#btnFechar");
+
+  //Armazenar os dados que chegam do form em um objeto:
+  const userForm = {
+    email: email.value,
+    senha: senha.value
+  }
+
+  botaoModal.addEventListener("click", function () {
+      modalDialog.close();
+      modalDialog.classList.toggle("modal-danger");
+    });
+
+  //Validação
+  try {
+
+    if (usuarioDb) {
+
+      if ((usuarioDb.email === userForm.email) && (usuarioDb.senha === userForm.senha)) {
+
+        //Abrindo o dialog e deixando o fundo livre.
+        // modalDialog.show();
+
+        //Abrindo o dialog e travando o fundo.
+        modalDialog.showModal();
+
+        //Adicionar a mensagem de sucesso para o usuário no modal!
+        //Vamos usar o innerHTML para injetar 2 <p> com o texto na div=msg.
+        //Mas para isso necessitamos capturar a div=msg antes
+
+        let contador = 5;
+
+        //Injetando os 2 <p> com a mensagem de sucesso.
+        divMsg.innerHTML = `<p>Login realizado com Sucesso!</p><p>Você será redirecionado em ${contador} segundos...</p>`;
+
+        //Temporizador de redirecionamento com setInterval.
+        const intervalo = setInterval(function () {
+
+          contador--;
+          divMsg.innerHTML = `<p>Login realizado com Sucesso!</p><p>Você será redirecionado em ${contador} segundos...</p>`;
+
+          if (contador === 0) {
+            clearInterval(intervalo);
+            //Redirecionamento do usuário para uma nova página!!
+            window.location.href = "../index.html";
+          }
+
+        }, 1000);
+
+      } else {
+        throw Error("Email ou Senha incorretos.");
+      }
+
+    } else {
+      throw Error("Ocorreu um erro no acesso as informações do usuário.");
     }
 
-    //Validação
-    try {
-
-        if (usuarioDb) {
-
-            if( (usuarioDb.email === userForm.email) && (usuarioDb.senha === userForm.senha)){
-                //Capturar o dialog
-                const modalDialog = document.querySelector("#meuModal");
-
-                //Abrindo o dialog e deixando o fundo livre.
-                // modalDialog.show();
-
-                //Abrindo o dialog e travando o fundo.
-                modalDialog.showModal();
-
-                
-                //Capturar o botão do modal para atrelar nele o evento de click e assim colocar  neste botão a ação de fechar o modal.
-                const botaoModal = document.querySelector("#btnFechar");
-                botaoModal.addEventListener("click", function(){
-                    modalDialog.close();
-                });
-                
-                //Adicionar a mensagem de sucesso para o usuário no modal!
-                //Vamos usar o innerHTML para injetar 2 <p> com o texto na div=msg.
-                //Mas para isso necessitamos capturar a div=msg antes
-                const divMsg = document.querySelector("#msg");
-                
-                let contador = 5;
-
-                //Injetando os 2 <p> com a mensagem de sucesso.
-                divMsg.innerHTML = `<p>Login realizado com Sucesso!</p><p>Você será redirecionado em ${contador} segundos...</p>`;
-
-                //Temporizador de redirecionamento com setInterval.
-                const intervalo = setInterval( function(){
-                    
-                    contador--;
-                    divMsg.innerHTML = `<p>Login realizado com Sucesso!</p><p>Você será redirecionado em ${contador} segundos...</p>`;
-                    
-                    if(contador === 0){
-                        clearInterval(intervalo);
-                        //Redirecionamento do usuário para uma nova página!!
-                        window.location.href = "../index.html";
-                    }
-
-                }, 1000 );
-
-            }else{
-                throw Error("Email ou Senha incorretos.");
-            }
-            
-        }else{
-            throw Error("Ocorreu um erro no acesso as informações do usuário.");
-        }
-
-    } catch (error) {
-        console.error(error);
-        alert(error);
-    }
+  } catch (error) {
+    modalDialog.showModal();
+    modalDialog.toggleAttribute("open");
+    modalDialog.classList.toggle("modal-danger")
+    divMsg.innerHTML = `<p>${error.message}</p>`;
+    console.error(error);
+  }
 
 });
 
